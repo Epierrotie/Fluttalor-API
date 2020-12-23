309810970d6b98f547e694c3e7f03f1b045fbcf3
Pierre Leriche <pierre.leriche@epitech.eu>
2020-12-23 16:46:14 +0100

    chores : try heroku postgres

--- api/settings.py
+++ api/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 from datetime import timedelta
 from os.path import join, dirname
 
+import django_heroku
+
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'xy*ehd7rk)-y#ukos@jmb*p!odj1%bg)ulztjig8w9x(47unnc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
-    "fluttalor.herokuapp.com"
+    "fluttalor.heroku.com"
 ]
 
 # Application definition
@@ -97,12 +101,12 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
