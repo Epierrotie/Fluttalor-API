38674a647a845a69e27124f1eb5741c65750ae3d
Pierre Leriche <pierre.leriche@epitech.eu>
2020-12-28 17:48:31 +0100

    chores : remove debug

--- api/settings.py
+++ api/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
-    "fluttalor.heroku.com",
-    "localhost"
+    "fluttalor.heroku.com"
 ]
 
 # Application definition
