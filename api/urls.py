d001d15cdd0af3895018dc264827c5ffb86afb4e
Pierre Leriche <pierre.leriche@epitech.eu>
2020-12-28 14:44:11 +0100

    test3

--- api/urls.py
+++ api/urls.py
@@ -42,4 +42,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-] + router.urls + static('staticfiles', document_root=settings.STATIC_ROOT)
+] + router.urls + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
